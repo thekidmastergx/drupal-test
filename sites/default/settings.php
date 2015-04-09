31b20041593b69621352e48c11847c8e451ad62d
thekidmastergx <thekidmastergx@gmail.com>
2015-04-09 06:40:50 +0000

    Koding Push

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -88,7 +88,7 @@
  *   $db_url = 'mysqli://username:password@localhost/databasename';
  *   $db_url = 'pgsql://username:password@localhost/databasename';
  */
-$db_url = 'mysql://username:password@localhost/databasename';
+$db_url = 'mysqli://druser:password@localhost/drupal';
 $db_prefix = '';
 
 /**
